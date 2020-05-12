c3cc1f822b272bc998dc35a40c4a87ffc398be16
ku-mu <cocodrips@gmail.com>
2020-05-12 17:17:09 +0900

    Add token parser.

--- src/_token.py
+++ src/_token.py
@@ -1,14 +1,17 @@
 import dataclasses
 import enum
+import re
+
+symbols_pattern = re.compile(r"[+=]")
+delimiter = re.compile(r"[\s\t]")
 
 
 class TokenType(enum.Enum):
     TOK_IDENTIFIER = 1
     TOK_DIGIT = 2
     TOK_SYMBOL = 3
-    TOK_INT = 4
-    TOK_RETURN = 5
-    TOK_EOF = 6
+
+    TOK_PRINT = 100
 
 
 @dataclasses.dataclass
@@ -18,7 +21,28 @@ class Token:
     number: int
     line: int
 
+    @classmethod
+    def init_from_string(cls, token_string: str, line: int):
+        token_string = token_string
+        number = None
+        token_type = TokenType.TOK_IDENTIFIER
+
+        if token_string == "print":
+            token_type = TokenType.TOK_PRINT
+        elif token_string.isdigit():
+            number = int(token_string)
+            token_type = TokenType.TOK_DIGIT
+        elif token_string.isalnum():
+            token_type = TokenType.TOK_IDENTIFIER
+        if symbols_pattern.match(token_string):
+            token_type = TokenType.TOK_SYMBOL
+
+        return Token(token_type, token_string, number, line)
+
 
 class TokenStream:
     tokens: list = []
-    current_index: int = 1
+    current_index: int = 0
+
+    def __repr__(self):
+        return f"index: {self.current_index} tokens: {self.tokens}"
--- src/lexer.py
+++ src/lexer.py
@@ -1,13 +1,39 @@
 from src._token import *
 
+
 def main(filepath):
     print("Target file", filepath)
 
     stream = TokenStream()
     with open(filepath, 'r') as f:
-        for line in f:
+
+        for line_num, line in enumerate(f):
+            iscomment = False
+            token_string = ""
+
             for c in line:
-                print(c)
+                if iscomment:
+                    raise NotImplementedError()
+
+                elif c.isalnum() or symbols_pattern.match(c):
+                    token_string = token_string + c
+
+                elif delimiter.match(c):
+                    if not token_string:
+                        continue
+                    token = Token.init_from_string(token_string, line_num)
+                    stream.tokens.append(token)
+                    token_string = ""
+                else:
+                    raise Exception(f"Unsupported character: {c}")
+
+            # Last token
+            if token_string:
+                token = Token.init_from_string(token_string, line_num)
+                stream.tokens.append(token)
+
+    for token in stream.tokens:
+        print(token)
 
 
 if __name__ == '__main__':
